8cf141d8dcdcfa7d76a5288f08caf91fd9ca0bd8
Euan <euank@euank.com>
2015-12-22 21:22:55 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
 # go-errstats
-Go program to print err-check statistics about Go code
+
+```
+<montgc> I wonder what percentage of all Go code is "if err != nil"
+```
+
+It's fairly common to hear complaints that Go's error handling is verbose, unwieldy, verbose, and verbose.
+I don't want to get into that argument here, other than to supply data.
+
+## What does this do?
+
+This is a simple program to parse your Go programs (caveat: will only work if
+your program already compiles, might not play nice with build flags) and figure
+out what percent of your code is `if err != nil` conditionals.
+
+## What doesn't it work on?
+
+Interesting tidbit, when you do `err := recover(); err != nil`, the type of
+`err` there is `interface{}`, so this program doesn't catch that one.
+
+Most importantly, this program doesn't currently handle compound conditionals (e.g. `if err != nil && foo != bar`) and is thus liable to undercount.
+
+It also doesn't handle build tags nor any other such fancery.
+
+If you know of other issues or want to fix any of these, issues and pull requests are quite welcome.
+
+##  Sample outputs
+
+I arbitrarily picked a few packages I had lying around to get some sample output:
+
+### Docker
+
+```
+$ git rev-parse --short HEAD
+312c826
+$ errstats $(sort <(go list ./...) <(go list ./vendor/... ./contrib/...) | uniq -u | grep -v "integration-cli")
+Statistics about your go files:
+  Total lines:  72028
+  Total meaningful lines:   40578
+  Total expressions:  276800
+  Total conditionals:   6427
+  Total conditionals that were error checks:  2781
+
+  Percent lines that were errchecks:  6.85346739612598
+  Percent expressions that were errchecks:  1.0046965317919074
+  Percent conditionals that were errchecks:   43.27057725221721
+  Percent of err != nil checks using the var 'err':   98.92125134843582
+```
+
+## Logrus
+```
+$ git rev-parse --short HEAD
+446d1c1
+$ errstats $(go list github.com/Sirupsen/logrus/...)
+Statistics about your go files:
+  Total lines:  1277
+  Total meaningful lines:   661
+  Total expressions:  4768
+  Total conditionals:   68
+  Total conditionals that were error checks:  9
+
+  Percent lines that were errchecks:  1.361573373676248
+  Percent expressions that were errchecks:  0.18875838926174499
+  Percent conditionals that were errchecks:   13.23529411764706
+  Percent of err != nil checks using the var 'err':   100
+```
+
+## Martini
+```
+$ git rev-parse --short HEAD
+15a4762
+$ errstats $(go list github.com/go-martini/martini/...)
+Statistics about your go files:
+  Total lines:  1088
+  Total meaningful lines:   595
+  Total expressions:  4335
+  Total conditionals:   67
+  Total conditionals that were error checks:  8
+
+  Percent lines that were errchecks:  1.3445378151260505
+  Percent expressions that were errchecks:  0.1845444059976932
+  Percent conditionals that were errchecks:   11.940298507462686
+  Percent of err != nil checks using the var 'err':   100
+```
+## http2
+```
+$ git rev-parse --short HEAD
+6c89489
+$ errstats $(go list golang.org/x/net/http2/...)
+Statistics about your go files:
+  Total lines:  8072
+  Total meaningful lines:   4652
+  Total expressions:  29802
+  Total conditionals:   636
+  Total conditionals that were error checks:  79
+
+  Percent lines that were errchecks:  1.6981943250214964
+  Percent expressions that were errchecks:  0.2650828803436011
+  Percent conditionals that were errchecks:   12.421383647798741
+  Percent of err != nil checks using the var 'err':   98.73417721518987
+```
+
+## ecs-agent
+```
+$ git checkout v1.7.0
+$ cat <(. ./scripts/shared_env; errstats $(go list github.com/aws/amazon-ecs-agent/agent/...))
+Statistics about your go files:
+  Total lines:  19539
+  Total meaningful lines:   9346
+  Total expressions:  64451
+  Total conditionals:   807
+  Total conditionals that were error checks:  217
+
+  Percent lines that were errchecks:  2.3218489193237746
+  Percent expressions that were errchecks:  0.33668988844238257
+  Percent conditionals that were errchecks:   26.889714993804215
+  Percent of err != nil checks using the var 'err':   97.6958525345622
+```
---
+++ errstats.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"go/ast"
+	"go/build"
+	"go/printer"
+	"go/token"
+	"strconv"
+
+	"github.com/Sirupsen/logrus"
+
+	"golang.org/x/tools/go/types"
+
+	"golang.org/x/tools/go/loader"
+)
+
+// ty https://github.com/kisielk/errcheck/blob/master/internal/errcheck/errcheck.go
+var errorType *types.Interface
+
+func init() {
+	errorType = types.Universe.Lookup("error").Type().Underlying().(*types.Interface)
+}
+
+type errStatVisitor struct {
+	lineCount              int64
+	expressionCount        int64
+	conditionCount         int64
+	errNotNilCount         int64
+	errNotNilNamedErrCount int64
+	nilNilCount            int64
+
+	verbose      bool
+	pkgInfo      *loader.PackageInfo
+	fset         *token.FileSet
+	exprLinesMap map[string]struct{}
+}
+
+func percent(lhs, rhs int64) float64 {
+	if rhs == 0 {
+		return 0
+	}
+	return float64(lhs) / float64(rhs) * 100.0
+}
+
+func (e *errStatVisitor) PrettyPrint() {
+	output :=
+		fmt.Sprintf("Statistics about your go files:\n"+
+			"\tTotal lines: \t%v\n"+
+			"\tTotal meaningful lines: \t%v\n"+
+			"\tTotal expressions: \t%v\n"+
+			"\tTotal conditionals: \t%v\n"+
+			"\tTotal conditionals that were error checks: \t%v\n"+
+			"\n"+
+			"\tPercent lines that were errchecks: \t%v\n"+
+			"\tPercent expressions that were errchecks: \t%v\n"+
+			"\tPercent conditionals that were errchecks: \t%v\n"+
+			"\tPercent of err != nil checks using the var 'err': \t%v\n",
+			e.lineCount,
+			len(e.exprLinesMap),
+			e.expressionCount,
+			e.conditionCount,
+			e.errNotNilCount,
+			percent(e.errNotNilCount, int64(len(e.exprLinesMap))),
+			percent(e.errNotNilCount, e.expressionCount),
+			percent(e.errNotNilCount, e.conditionCount),
+			percent(e.errNotNilNamedErrCount, e.errNotNilCount),
+		)
+
+	if e.nilNilCount > 0 {
+		output = fmt.Sprintf("\tNumber of 'nil != nil' and 'nil == nil' conditionals. FIX THIS: \t%v\n", e.nilNilCount)
+	}
+
+	fmt.Printf(output)
+
+}
+
+func (e *errStatVisitor) Visit(node ast.Node) ast.Visitor {
+	if node == nil {
+		return nil
+	}
+
+	pos := e.fset.Position(node.Pos())
+	line := pos.Filename + ":" + strconv.Itoa(pos.Line)
+	e.exprLinesMap[line] = struct{}{}
+
+	handleCond := func(cond ast.Expr) {
+		logrus.Debug("line: " + line)
+		var logbuf bytes.Buffer
+		printer.Fprint(&logbuf, e.fset, node)
+		logrus.Debug("node: " + logbuf.String())
+
+		if neq, ok := cond.(*ast.BinaryExpr); ok {
+			if _, isIdent := neq.X.(*ast.Ident); !isIdent {
+				// e.g. ast.SelectorExpr
+				logrus.Debug("skipping, lhs isn't good for us")
+				return
+			}
+			if _, isIdent := neq.Y.(*ast.Ident); !isIdent {
+				logrus.Debug("skipping, rhs isn't good for us")
+				return
+			}
+
+			if neq.X.(*ast.Ident).Name == "nil" &&
+				neq.Y.(*ast.Ident).Name == "nil" {
+				logrus.Warn("line " + line + " has a double nil check")
+				e.nilNilCount++
+				return
+			}
+
+			if neq.Op == token.NEQ {
+				var el *ast.Ident
+				if neq.X.(*ast.Ident).Name == "nil" {
+					el = neq.Y.(*ast.Ident)
+				} else if neq.Y.(*ast.Ident).Name == "nil" {
+					el = neq.X.(*ast.Ident)
+				} else {
+					// Neither half nil, whatever
+					return
+				}
+
+				thisErr := e.pkgInfo.Types[el]
+				if types.Implements(thisErr.Type, errorType) {
+					logrus.Debug("identified 'err' type")
+					e.errNotNilCount++
+
+					if el.Name == "err" {
+						logrus.Debug("identified 'err' name")
+						e.errNotNilNamedErrCount++
+					}
+				}
+			}
+		}
+	}
+
+	e.expressionCount++
+	switch stmnt := node.(type) {
+	case *ast.ForStmt:
+		logrus.Debug("for statement")
+		if stmnt.Cond != nil {
+			e.conditionCount++
+			handleCond(stmnt.Cond)
+		}
+	case *ast.IfStmt:
+		logrus.Debug("if statement")
+		e.conditionCount++
+		handleCond(stmnt.Cond)
+	}
+
+	return e
+}
+
+func main() {
+	loglevel := flag.String("loglevel", "info", "loglevel")
+	allFiles := flag.Bool("all", false, "stats for all dependencies")
+	flag.Parse()
+
+	logLvl, err := logrus.ParseLevel(*loglevel)
+	if err != nil {
+		logrus.Fatalf("Cannot parse level: %v", loglevel)
+	}
+	logrus.SetLevel(logLvl)
+
+	loadcfg := loader.Config{Build: &build.Default}
+	loadcfg.FromArgs(flag.Args(), false)
+
+	program, err := loadcfg.Load()
+	if err != nil {
+		logrus.Fatalf("error loading stuff %v\n", err)
+	}
+
+	v := &errStatVisitor{
+		fset:         program.Fset,
+		exprLinesMap: make(map[string]struct{}),
+	}
+
+	var pkgs []*loader.PackageInfo
+	if *allFiles {
+		pkgs = pkgSlice(program.AllPackages)
+	} else {
+		pkgs = program.InitialPackages()
+	}
+
+	for _, pkg := range pkgs {
+		v.pkgInfo = pkg
+		for _, astFile := range pkg.Files {
+			logrus.WithFields(logrus.Fields{
+				"file":    v.fset.Position(astFile.Pos()).Filename,
+				"package": pkg.String(),
+			}).Debug()
+
+			v.lineCount += int64(v.fset.File(astFile.Pos()).LineCount())
+			ast.Walk(v, astFile)
+		}
+	}
+
+	v.PrettyPrint()
+}
+
+func pkgSlice(m map[*types.Package]*loader.PackageInfo) []*loader.PackageInfo {
+	output := make([]*loader.PackageInfo, len(m))
+	i := 0
+	for _, el := range m {
+		output[i] = el
+		i++
+	}
+	return output
+}
