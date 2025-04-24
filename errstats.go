c8a8d20b8aac4c6d1ff2fb34f25938a589f526c9
Euan Kemp <euank@euank.com>
2025-04-24 13:26:43 +0900

    Update for go modules

--- errstats.go
+++ errstats.go
@@ -5,14 +5,14 @@ import (
 	"flag"
 	"fmt"
 	"go/ast"
-	"go/build"
 	"go/printer"
 	"go/token"
 	"go/types"
+	"log/slog"
+	"os"
 	"strconv"
 
-	"github.com/Sirupsen/logrus"
-	"golang.org/x/tools/go/loader"
+	"golang.org/x/tools/go/packages"
 )
 
 // ty https://github.com/kisielk/errcheck/blob/master/internal/errcheck/errcheck.go
@@ -31,7 +31,7 @@ type errStatVisitor struct {
 	nilNilCount            int64
 
 	verbose      bool
-	pkgInfo      *loader.PackageInfo
+	pkgInfo      *packages.Package
 	fset         *token.FileSet
 	exprLinesMap map[string]struct{}
 }
@@ -44,35 +44,33 @@ func percent(lhs, rhs int64) float64 {
 }
 
 func (e *errStatVisitor) PrettyPrint() {
-	output :=
-		fmt.Sprintf("Statistics about your go files:\n"+
-			"\tTotal lines: \t%v\n"+
-			"\tTotal meaningful lines: \t%v\n"+
-			"\tTotal expressions: \t%v\n"+
-			"\tTotal conditionals: \t%v\n"+
-			"\tTotal conditionals that were error checks: \t%v\n"+
-			"\n"+
-			"\tPercent lines that were errchecks: \t%v\n"+
-			"\tPercent expressions that were errchecks: \t%v\n"+
-			"\tPercent conditionals that were errchecks: \t%v\n"+
-			"\tPercent of err != nil checks using the var 'err': \t%v\n",
-			e.lineCount,
-			len(e.exprLinesMap),
-			e.expressionCount,
-			e.conditionCount,
-			e.errNotNilCount,
-			percent(e.errNotNilCount, int64(len(e.exprLinesMap))),
-			percent(e.errNotNilCount, e.expressionCount),
-			percent(e.errNotNilCount, e.conditionCount),
-			percent(e.errNotNilNamedErrCount, e.errNotNilCount),
-		)
+	output := fmt.Sprintf("Statistics about your go files:\n"+
+		"\tTotal lines: \t%v\n"+
+		"\tTotal meaningful lines: \t%v\n"+
+		"\tTotal expressions: \t%v\n"+
+		"\tTotal conditionals: \t%v\n"+
+		"\tTotal conditionals that were error checks: \t%v\n"+
+		"\n"+
+		"\tPercent lines that were errchecks: \t%v\n"+
+		"\tPercent expressions that were errchecks: \t%v\n"+
+		"\tPercent conditionals that were errchecks: \t%v\n"+
+		"\tPercent of err != nil checks using the var 'err': \t%v\n",
+		e.lineCount,
+		len(e.exprLinesMap),
+		e.expressionCount,
+		e.conditionCount,
+		e.errNotNilCount,
+		percent(e.errNotNilCount, int64(len(e.exprLinesMap))),
+		percent(e.errNotNilCount, e.expressionCount),
+		percent(e.errNotNilCount, e.conditionCount),
+		percent(e.errNotNilNamedErrCount, e.errNotNilCount),
+	)
 
 	if e.nilNilCount > 0 {
 		output = fmt.Sprintf("\tNumber of 'nil != nil' and 'nil == nil' conditionals. FIX THIS: \t%v\n", e.nilNilCount)
 	}
 
-	fmt.Printf(output)
-
+	fmt.Print(output)
 }
 
 func (e *errStatVisitor) Visit(node ast.Node) ast.Visitor {
@@ -85,25 +83,25 @@ func (e *errStatVisitor) Visit(node ast.Node) ast.Visitor {
 	e.exprLinesMap[line] = struct{}{}
 
 	handleCond := func(cond ast.Expr) {
-		logrus.Debug("line: " + line)
+		slog.Debug("line: " + line)
 		var logbuf bytes.Buffer
 		printer.Fprint(&logbuf, e.fset, node)
-		logrus.Debug("node: " + logbuf.String())
+		slog.Debug("node: " + logbuf.String())
 
 		if neq, ok := cond.(*ast.BinaryExpr); ok {
 			if _, isIdent := neq.X.(*ast.Ident); !isIdent {
 				// e.g. ast.SelectorExpr
-				logrus.Debug("skipping, lhs isn't good for us")
+				slog.Debug("skipping, lhs isn't good for us")
 				return
 			}
 			if _, isIdent := neq.Y.(*ast.Ident); !isIdent {
-				logrus.Debug("skipping, rhs isn't good for us")
+				slog.Debug("skipping, rhs isn't good for us")
 				return
 			}
 
 			if neq.X.(*ast.Ident).Name == "nil" &&
 				neq.Y.(*ast.Ident).Name == "nil" {
-				logrus.Warn("line " + line + " has a double nil check")
+				slog.Warn("line " + line + " has a double nil check")
 				e.nilNilCount++
 				return
 			}
@@ -119,13 +117,17 @@ func (e *errStatVisitor) Visit(node ast.Node) ast.Visitor {
 					return
 				}
 
-				thisErr := e.pkgInfo.Types[el]
-				if types.Implements(thisErr.Type, errorType) {
-					logrus.Debug("identified 'err' type")
+				thisErr := e.pkgInfo.TypesInfo.Uses[el]
+				if thisErr == nil || thisErr.Type() == nil {
+					slog.Debug("could not find type for el", "el", el)
+					return
+				}
+				if types.Implements(thisErr.Type(), errorType) {
+					slog.Debug("identified 'err' type")
 					e.errNotNilCount++
 
 					if el.Name == "err" {
-						logrus.Debug("identified 'err' name")
+						slog.Debug("identified 'err' name")
 						e.errNotNilNamedErrCount++
 					}
 				}
@@ -136,13 +138,13 @@ func (e *errStatVisitor) Visit(node ast.Node) ast.Visitor {
 	e.expressionCount++
 	switch stmnt := node.(type) {
 	case *ast.ForStmt:
-		logrus.Debug("for statement")
+		slog.Debug("for statement")
 		if stmnt.Cond != nil {
 			e.conditionCount++
 			handleCond(stmnt.Cond)
 		}
 	case *ast.IfStmt:
-		logrus.Debug("if statement")
+		slog.Debug("if statement")
 		e.conditionCount++
 		handleCond(stmnt.Cond)
 	}
@@ -151,58 +153,51 @@ func (e *errStatVisitor) Visit(node ast.Node) ast.Visitor {
 }
 
 func main() {
+	if err := main_(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+}
+
+func main_() error {
 	loglevel := flag.String("loglevel", "info", "loglevel")
 	allFiles := flag.Bool("all", false, "stats for all dependencies")
 	flag.Parse()
 
-	logLvl, err := logrus.ParseLevel(*loglevel)
+	var lvl slog.Level
+	err := lvl.UnmarshalText([]byte(*loglevel))
 	if err != nil {
-		logrus.Fatalf("Cannot parse level: %v", loglevel)
+		slog.Error("invalid log level", "err", err)
+		os.Exit(1)
 	}
-	logrus.SetLevel(logLvl)
+	l := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: lvl,
+	}))
+	slog.SetDefault(l)
 
-	loadcfg := loader.Config{Build: &build.Default}
-	loadcfg.FromArgs(flag.Args(), false)
-
-	program, err := loadcfg.Load()
+	loadcfg := &packages.Config{Mode: packages.LoadSyntax}
+	if *allFiles {
+		loadcfg.Mode = packages.LoadAllSyntax
+	}
+	pkgs, err := packages.Load(loadcfg, flag.Args()...)
 	if err != nil {
-		logrus.Fatalf("error loading stuff %v\n", err)
+		return err
 	}
 
 	v := &errStatVisitor{
-		fset:         program.Fset,
 		exprLinesMap: make(map[string]struct{}),
-	}
-
-	var pkgs []*loader.PackageInfo
-	if *allFiles {
-		pkgs = pkgSlice(program.AllPackages)
-	} else {
-		pkgs = program.InitialPackages()
 	}
 
 	for _, pkg := range pkgs {
 		v.pkgInfo = pkg
-		for _, astFile := range pkg.Files {
-			logrus.WithFields(logrus.Fields{
-				"file":    v.fset.Position(astFile.Pos()).Filename,
-				"package": pkg.String(),
-			}).Debug()
-
+		v.fset = pkg.Fset
+		for _, astFile := range pkg.Syntax {
+			slog.Debug("parsing", "file", v.fset.Position(astFile.Pos()).Filename, "package", pkg.String())
 			v.lineCount += int64(v.fset.File(astFile.Pos()).LineCount())
 			ast.Walk(v, astFile)
 		}
 	}
 
 	v.PrettyPrint()
-}
-
-func pkgSlice(m map[*types.Package]*loader.PackageInfo) []*loader.PackageInfo {
-	output := make([]*loader.PackageInfo, len(m))
-	i := 0
-	for _, el := range m {
-		output[i] = el
-		i++
-	}
-	return output
+	return nil
 }
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/euank/go-errstats
+
+go 1.24.1
+
+require golang.org/x/tools v0.32.0
+
+require (
+	golang.org/x/mod v0.24.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+)
